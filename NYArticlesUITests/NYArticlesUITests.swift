130cd9f3a99d11e5c6579c9594014cf0a2a4d699
Hem Sharma <hem.sharma@nagarro.com>
2018-07-05 11:53:15 +0530

    Implemented UI test cases

--- NYArticlesUITests/NYArticlesUITests.swift
+++ NYArticlesUITests/NYArticlesUITests.swift
@@ -21,6 +21,7 @@ class NYArticlesUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         app = XCUIApplication()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -31,18 +32,43 @@ class NYArticlesUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testExample()
+    {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
     func testArticleTableInteraction()
-    {
-        app.launch()
-        
+    {        
         // Assert that we are displaying the tableview
         let articleTableView = app.tables["NYCArticleTableView"]
         XCTAssertTrue(articleTableView.exists, "Article tableview exists")
+        
+        
+        // Get an array of cells
+        let promise = expectation(description: "Wait for table cells")
+        let tableCells = articleTableView.cells
+        if tableCells.count > 0
+        {
+            promise.fulfill()
+            // Grab the first cell and verify that it exists and tap it
+            let tableCell = tableCells.element(boundBy: 0)
+            XCTAssertTrue(tableCell.exists, "The cell exist on the table")
+            
+            // Does this actually take us to the next screen
+            tableCell.tap()
+            
+            // Back
+            app.navigationBars.buttons.element(boundBy: 0).tap()
+            
+            XCTAssertTrue(true, "Finished validating the table cells")
+        }
+        else
+        {
+            XCTAssert(false, "Was not able to find any table cells")
+        }
+        
+        waitForExpectations(timeout: 20, handler: nil)
     }
     
 }
